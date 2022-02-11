37a58cb9fc3dc2d9a1d28455f49e87ac99e662f6
jeromeBRM <54836707+jeromeBRM@users.noreply.github.com>
2022-02-11 10:19:25 +0100

    liste users

--- www/views/home.php
+++ www/views/home.php
@@ -2,6 +2,8 @@
 
 <p>Coucou ! Voici la liste des utilisateurs :</p>
 
+<ul>
+
 <?php
 
 global $pdo;
@@ -9,5 +11,12 @@ global $pdo;
 $sql = $pdo->query("SELECT * FROM User");
 $users = $sql ->FetchAll();
 foreach($users as $user){
-	echo $user["Login"];
+?>
+<li>
+    <?php echo $user["Login"]; ?>
+</li>
+<?php
 }
+?>
+
+</ul>
