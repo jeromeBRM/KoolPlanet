a4b132d898b9611fb90ae7c0c7f8ee6d5b507f64
jeromeBRM <54836707+jeromeBRM@users.noreply.github.com>
2022-02-11 15:26:34 +0100

    Update template.php

--- www/views/template.php
+++ www/views/template.php
@@ -13,7 +13,7 @@
                 <ul>
                     <li><a href="?action=login">Se connecter</a></li>
                     <li><a href="?action=signup">S'inscrire</a></li>
-                    <li><a href="?action=signup">Se déconnecter</a></li>
+                    <li><a href="">Se déconnecter</a></li>
                 </ul>
                 <h5 class="header__info--login"><?= "Vous êtes connecté(e) sur le compte de " . $_SESSION["login"]; ?></h5>
             </nav>
