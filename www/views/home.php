7d0c117ea3912b3c63864e3ba80b6c58a4efa3cc
amemelili <91413456+amemelili@users.noreply.github.com>
2022-02-11 10:11:53 +0100

    Update home.php

--- www/views/home.php
+++ www/views/home.php
@@ -2,3 +2,10 @@
 
 <p>Coucou ! Voici la liste des utilisateurs :</p>
 
+<?php
+$sql = $PDO ->query"SELECT * FROM users");
+$user = $sql ->FetchAll()
+foreach($users as $user){
+		print_r($user);
+		echo PHP_SQL;
+}
