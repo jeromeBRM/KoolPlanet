66c20cf215f516efcd5c170358ae766b9b6d146e
jeromeBRM <54836707+jeromeBRM@users.noreply.github.com>
2022-02-11 13:19:11 +0100

    Update loginController.php

--- www/controllers/loginController.php
+++ www/controllers/loginController.php
@@ -1,7 +1,10 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
+if ($_SERVER["REQUEST_METHOD"] != "POST") {
+    echo "laisse moi zoom zoom zeng dans ton<br>";
+    echo "<img src='https://pbs.twimg.com/media/EElFMaHXkAESNCM.jpg' width='200px'>";
+    exit(1);
 }
-if (!isset($_POST["login"] || !isset($_POST["password"])))
+if (!isset($_POST["login"]) || !isset($_POST["password"]))
 
 
 $sql = $pdo->query(" SELECT DISTINCT Login FROM User WHERE Login = login;");
@@ -12,6 +15,5 @@ $data = array (
     "login" => "",
     "password" => ""
 );
-
 
 render("login", $data);
