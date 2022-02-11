2a2910fe71e92c13aad81a7e8a406403be932911
jeromeBRM <54836707+jeromeBRM@users.noreply.github.com>
2022-02-11 13:36:02 +0100

    d

--- www/controllers/signupController.php
+++ www/controllers/signupController.php
@@ -1,5 +1,32 @@
 <?php
 
+if (isset($_POST["login"]) && isset($_POST["password"]) && isset($_POST["email"]) && isset($_POST["confirmation"])){
+
+    // l'utilisateur a correctement saisi le formulaire
+
+    $login = $_POST["login"];
+    $password = $_POST["password"];
+    $email = $_POST["email"];
+    $confirmation = $_POST["confirmation"];
+
+    if ($password == $confirmation){
+
+        // l'utilisateur a correctement saisi son mot de passe
+
+        global $pdo;
+
+        $sql = $pdo->prepare(
+            'INSERT INTO User VALUES (?, ?, ?)'
+            );
+        $sql->execute(array($login, $email, $password));
+
+        echo "vous êtes inscrit !";
+    }
+    else{
+        echo "veuillez confirmer votre mot de passe";
+    }
+}
+
 $data = array (
 );
 
--- www/views/signup.php
+++ www/views/signup.php
@@ -1,40 +1,3 @@
-<?php
-
-if (isset($_SESSION["err"])){
-	$err = $_SESSION["err"];
-	unset($_SESSION["err"]);
-}
-
-if (isset($_POST["login"]) && isset($_POST["password"]) && isset($_POST["email"]) && isset($_POST["confirmation"])){
-
-    // l'utilisateur a correctement saisi le formulaire
-
-    $login = $_POST["login"];
-    $password = $_POST["password"];
-    $email = $_POST["email"];
-    $confirmation = $_POST["confirmation"];
-
-    if ($password == $confirmation){
-
-        // l'utilisateur a correctement saisi son mot de passe
-
-        global $pdo;
-
-        $sql = $pdo->prepare(
-            'INSERT INTO User VALUES (?, ?, ?)'
-            );
-        $sql->execute(array($login, $email, $password));
-
-        echo "vous êtes inscrit !";
-    }
-    else{
-        echo "veuillez confirmer votre mot de passe";
-    }
-}
-
-else{
-?>
-
 <form method = "POST" action = '?action=signup'>
 	<h1> Formulaire d'inscription
 		<h3> Email
@@ -52,6 +15,7 @@ else{
 			<input type = "password" name = "confirmation">
 		</h3>
 	</h1>	
+<<<<<<< Updated upstream
     <input type = "submit">
 </form>
 <?php } ?>
@@ -59,3 +23,7 @@ else{
 <?php if (isset ($err)) { ?>
 <p> <?php echo $err ; ?> </p>
 <?php } ?>
+=======
+<input type = "submit">
+</form>
+>>>>>>> Stashed changes
