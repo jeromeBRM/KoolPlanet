fea7c2297611bb8a2d199192c725c030bc4a0a46
amemelili <91413456+amemelili@users.noreply.github.com>
2022-02-11 10:52:41 +0100

    Update home.php

--- www/views/home.php
+++ www/views/home.php
@@ -1,15 +1,22 @@
 <p>Bienvenue</p>
 
-<p>Nombre d'utilisateurs inscrits : (2)</p>
+<p>Nombre d'utilisateurs inscrits : (
+    <?php
+
+    $sql = $pdo->query("SELECT DISTINCT Count(Login) as c FROM User 
+    ");
+    $users = $sql->fetch();
+    echo $users["c"]; 
+    ?>
+)</p>
 <p>Coucou ! Voici la liste des utilisateurs :</p>
 
 <ul>
 
 <?php
-global $pdo;
 
 $sql = $pdo->query("SELECT * FROM User");
-$users = $sql ->FetchAll();
+$users = $sql->fetchAll();
 foreach($users as $user){
 ?>
 <li>
