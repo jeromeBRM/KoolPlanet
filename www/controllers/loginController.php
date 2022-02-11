1ae4a4d7e18761936a7acb2db399a2eb95f412c5
amemelili <91413456+amemelili@users.noreply.github.com>
2022-02-11 13:14:13 +0100

    Update loginController.php

--- www/controllers/loginController.php
+++ www/controllers/loginController.php
@@ -3,15 +3,15 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 }
 if (!isset($_POST["login"] || !isset($_POST["password"])))
 
+
+$sql = $pdo->query(" SELECT DISTINCT Login FROM User WHERE Login = login;");
+$login = $sql->fetch();
+
 $data = array (
     "connected" => false,
     "login" => "",
     "password" => ""
 );
-
-// 1 ere requete sql qui cherche l'uilisateur login
-
-// SELECT DISTINCT Login FROM User WHERE Login = login;
 
 
 render("login", $data);
