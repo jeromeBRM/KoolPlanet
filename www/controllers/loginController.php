5e4bd206ca211ea361ada6a22655fc7a5656c2c4
amemelili <91413456+amemelili@users.noreply.github.com>
2022-02-11 11:44:26 +0100

    CTRLR

--- www/controllers/loginController.php
+++ www/controllers/loginController.php
@@ -1,6 +1,40 @@
 <?php
 
 $data = array (
+    "connected" => false,
+    "login" => "",
+    "password" => ""
 );
 
+// 1 ere requete sql qui cherche l'uilisateur login
+
+// SELECT * FROM User
+
+// deuxieme requete sql qui verifie si le mdp renseigné edt bien celui corresopndant au login
+
+
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+	if ($_POST["login"] !== "babar") {
+		echo "wrong login";
+	}
+	
+	if ($_POST["password"] !== "celeste") {
+		echo "wrong password";
+	}
+	
+	echo "ok";
+} else {
+
+}
+    
+echo '$_SERVER=';
+print_r($_SERVER);
+echo '$_POST=';
+print_r($_POST);
+echo '$_GET=';
+print_r($_GET);
+
+
 render("login", $data);
+
--- www/views/login.php
+++ www/views/login.php
@@ -1,34 +1,6 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-	if ($_POST["login"] !== "babar") {
-		echo "wrong login";
-	}
-	
-	if ($_POST["password"] !== "celeste") {
-		echo "wrong password";
-	}
-	
-	echo "ok";
-} else {
-?>
-
 <h1>Login Form</h1>
 <form method="POST" action="?action=login">
 	<input type="text" name="login">
 	<input type="password" name="password">
 	<input type="submit">
 </form>
-
-<?php
-}
-?>
-<!--
-<?php
-echo '$_SERVER=';
-print_r($_SERVER);
-echo '$_POST=';
-print_r($_POST);
-echo '$_GET=';
-print_r($_GET);
-?>
--->
