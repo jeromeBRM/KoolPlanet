e5278396e3b89ea3924f1b94014dd4cfeb50e21c
amemelili <91413456+amemelili@users.noreply.github.com>
2022-02-11 10:58:25 +0100

    j

--- www/controllers/homeController.php
+++ www/controllers/homeController.php
@@ -1,3 +1,20 @@
 <?php
 
 render("home");
+
+$sql = $pdo->query("SELECT DISTINCT Count(Login) as c FROM User 
+");
+ $users = $sql->fetch();
+echo $users["c"]; 
+
+$sql = $pdo->query("SELECT * FROM User");
+$users = $sql->fetchAll();
+foreach($users as $user){
+
+
+?>
+<li>
+    <?php echo $user["Login"]; ?>
+</li>
+<?php
+}
--- www/views/home.php
+++ www/views/home.php
@@ -1,29 +1,8 @@
 <p>Bienvenue</p>
 
-<p>Nombre d'utilisateurs inscrits : (
-    <?php
-
-    $sql = $pdo->query("SELECT DISTINCT Count(Login) as c FROM User 
-    ");
-    $users = $sql->fetch();
-    echo $users["c"]; 
-    ?>
-)</p>
+<p>Nombre d'utilisateurs inscrits : ()</p>
 <p>Coucou ! Voici la liste des utilisateurs :</p>
 
 <ul>
-
-<?php
-
-$sql = $pdo->query("SELECT * FROM User");
-$users = $sql->fetchAll();
-foreach($users as $user){
-?>
-<li>
-    <?php echo $user["Login"]; ?>
-</li>
-<?php
-}
-?>
 
 </ul>
