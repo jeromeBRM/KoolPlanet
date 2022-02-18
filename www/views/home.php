82cf80cb197bfa0b3bccd8b01a195405d056b188
amemelili <91413456+amemelili@users.noreply.github.com>
2022-02-18 15:02:05 +0100

    Update home.php

--- www/views/home.php
+++ www/views/home.php
@@ -9,4 +9,20 @@
             <?php echo $user["login"]; ?>
         </li>
     <?php } ?>
+
+    <?php foreach ($data["post_list"] as $post){ ?>
+        <li>
+            <?php
+            echo $post["id"];
+            echo $post["title"];
+            ?>
+        </li>
+    <?php } ?>
+
+    <form>
+    // titre + contenue avec un bouton return le formulaire avec le new contenue
+    </form>
+
 </ul>
+
+
