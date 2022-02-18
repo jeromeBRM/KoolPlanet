00f3a8f850d8c898e6ae2adc0878203749843165
Nerzah <93651948+Nerzah@users.noreply.github.com>
2022-02-18 15:00:20 +0100

    Update homeController.php

--- www/controllers/homeController.php
+++ www/controllers/homeController.php
@@ -6,7 +6,25 @@ $total = $sql->fetch();
 $sql = $db->query("SELECT * FROM kp_user");
 $users = $sql->fetchAll();
 
+$sql = $db->query("SELECT id, title FROM kp_post");
+$posts = $sql->fetchAll();
+
+/*
+        try{
+            $sql = $db->prepare(
+                'INSERT INTO kp_post VALUES (?, ?, ?)'
+                );
+            $sql->execute(array($data["login"], $data["email"], $hashedPassword));
+            $data["result"] = "Vous êtes inscrit !";
+        }
+        catch (\PDOException $e){
+            $data["result"] = $e->getMessage();
+        }
+    
+*/
+
 $data["user_list"] = $users;
 $data["user_total"] = $total["c"];
+$data["post_list"] = $posts;
 
 render("home", $data);
