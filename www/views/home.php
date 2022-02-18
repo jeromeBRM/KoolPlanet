88cbb58e17ac4f8cd8de91706212d3d032ffc789
amemelili <91413456+amemelili@users.noreply.github.com>
2022-02-18 16:17:19 +0100

    nananere

--- www/views/home.php
+++ www/views/home.php
@@ -1,35 +1,36 @@
 <p>Bienvenue</p>
 
-<p>Nombre d'utilisateurs inscrits : (<?php echo $data["user_total"]; ?>)</p>
+<p>Nombre d'utilisateurs inscrits : (<?= $data["user_total"]; ?>)</p>
 <p>Voici la liste des utilisateurs :</p>
 
 <ul>
     <?php foreach($data["user_list"] as $user){ ?>
         <li>
-            <?php echo $user["login"]; ?>
+            <?= $user["login"]; ?>
         </li>
     <?php } ?>
+</ul>
 
+<p> Voici la liste des topics :</p>
+<ul>
     <?php foreach ($data["post_list"] as $post){ ?>
         <li>
-            <?php
-            echo $post["id"];
-            echo $post["title"];
-            ?>
+            <a href="?action=post&id=<?= $post["id"]?>"><h4><?= $post["title"]; ?></h4></a>
         </li>
     <?php } ?>
-
-    <form method = "POST" action = '?action=home'>
-	<h1> Topic
-		<h3> Titre du Poste
-			<input type = "text" name = "post_title">
-		</h3>
-		<h3> Contenu du poste
-			<input type = "text" name = "post_content">
-		</h3>
-	</h1>	
-    <input type = "submit">
-    <p><?= $data["result"]; ?></p>
-</form>
-
 </ul>
+
+<div>
+    <form method = "POST" action = '?action=home'>
+        <h1> Topic
+            <h3> Titre du Poste
+                <input type = "text" name = "post_title">
+            </h3>
+            <h3> Contenu du poste
+                <input type = "text" name = "post_content">
+            </h3>
+        </h1>	
+        <input type = "submit">
+        <p><?= $data["result"]; ?></p>
+    </form>
+</div>
