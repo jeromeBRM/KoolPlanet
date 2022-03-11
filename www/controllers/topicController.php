093279602e8b2ab639ba209967f90301438a3ae5
jeromeBRM <54836707+jeromeBRM@users.noreply.github.com>
2022-03-11 13:00:29 +0100

    Système de réponse aux topics

--- www/controllers/topicController.php
+++ www/controllers/topicController.php
@@ -1,5 +1,33 @@
 <?php
+$data["form_complete"] = isset($_POST["reply_content"]);
 $data["result"] = "";
+
+if ($data["form_complete"]){
+    $data["form_complete"] = $_POST["reply_content"] != "";
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_SESSION["login"])) {
+
+    if ($data["form_complete"]){
+
+        $data["reply_content"] = $_POST["reply_content"];
+
+        try{
+            $sql = $db->prepare(
+                'insert into `reply` values (null, ?, ?, ?, datetime(\'now\', \'localtime\'))'
+                );
+            $sql->execute(array($_SESSION["id"], $_GET["id"], $data["reply_content"]));
+        }
+        catch (\PDOException $e){
+            $data["result"] = $e->getMessage();
+        }
+    }
+    else {
+        $data["result"] = "Il faut remplir tout les champs !";
+    }
+}
+
+unset($_POST);
 
 try{
     $sql = $db->prepare(
@@ -13,11 +41,23 @@ try{
     $sql->execute(array($_GET["id"]));
     $topic = $sql->fetch();
 
+    $sql = $db->prepare(
+        '
+        select login, content, posted_at from `reply`
+        inner join `user`
+        where `reply`.author_id = `user`.id
+        and `topic_id` = ?
+        '
+        );
+    $sql->execute(array($_GET["id"]));
+    $replies = $sql->fetchAll();
 }
 catch (\PDOException $e){
     $data["result"] = $e->getMessage();
 }
 
 render("topic", [
+    "result" => $data["result"],
     "topic" => $topic,
+    "replies" => $replies,
 ]);
--- www/views/topic.php
+++ www/views/topic.php
@@ -11,13 +11,26 @@
     <div>
         <?= $data["topic"]["posted_at"] ?>
     </div>
+</div>
+
+<div>
+<?php foreach($data["replies"] as $reply){ ?>
+    <p>
+        <?= $reply["content"] ?>
+    </p>
     <div>
-        <?php if (isset($_SESSION["login"])) { ?>
-            <form method = "POST" action = '?action=topic'>
-                <h3>Répondre</h3>
-                <textarea type = "text" name = "reply_content"></textarea>
-                <input type = "submit">
-            </form>
-        <?php } ?>
+        Posté par <?= $reply["login"] ?> le <?= $reply["posted_at"] ?>
     </div>
+<?php } ?>
+</div>
+
+<div>
+    <?php if (isset($_SESSION["login"])) { ?>
+        <form method = "POST" action = '?action=topic&id=<?= $_GET["id"]?>'>
+            <h3>Répondre</h3>
+            <textarea type = "text" name = "reply_content"></textarea>
+            <input type = "submit">
+        </form>
+        <?= $data["result"] ?>
+    <?php } ?>
 </div>
