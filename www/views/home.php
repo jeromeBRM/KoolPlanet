1ac771939c5ee719b1c03e59ab636c9790c60fd8
amemelili <91413456+amemelili@users.noreply.github.com>
2022-03-11 14:41:53 +0100

    css

--- www/views/home.php
+++ www/views/home.php
@@ -1,8 +1,13 @@
-<p>Bienvenue</p>
+<h2>Bienvenue sur KoolPlanet</h2>
+<p>Saviez vous que, chaque jour, on estime que 10 millions d’arbres sont coupés, soit 1/7 de la population 
+française? Saviez vous que l’ordinateur avec lequel vous regardez ce site consomme 200 kwh, soit 10 fois la production énergétique maximale 
+d’une éolienne domestique, et que chaque français génère en moyenne 5 tonnes de déchets par an ? Et pourtant, ces chiffres ne cessent d’augmenter.</p>
+<p>Le Forum Koolplanet constitue un lieu de rencontre et d’échange où il est question principalement 
+de la dimension environnementale du développement durable. Venez partager vos astuces ainsi que vos interrogations.</p>
+<p>Rejoignez le mouvement KoolPlanet !</p>
 
 <p>Nombre d'utilisateurs inscrits : (<?= $data["user_total"]; ?>)</p>
 <p>Voici la liste des utilisateurs :</p>
-
 <ul>
     <?php foreach($data["username_list"] as $user){ ?>
         <li>
