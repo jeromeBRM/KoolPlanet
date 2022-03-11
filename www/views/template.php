bd79426ced924ebb04dd4c4021f2f512ea8326f6
amemelili <91413456+amemelili@users.noreply.github.com>
2022-03-11 10:44:05 +0100

    Style.css

--- www/views/template.php
+++ www/views/template.php
@@ -5,6 +5,9 @@
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>KoolPlanet</title>
         <link rel="stylesheet" href="./src/style.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;1,300;1,400;1,500;1,600&display=swap" rel="stylesheet">
     </head>
     <body>
         <header>
