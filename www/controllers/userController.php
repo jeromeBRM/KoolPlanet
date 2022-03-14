cb79d1b4722f165eea70e511af0161903bb15e87
jeromeBRM <54836707+jeromeBRM@users.noreply.github.com>
2022-03-14 09:34:27 +0100

    Added avatar file upload to user profile

--- www/controllers/userController.php
+++ www/controllers/userController.php
@@ -1,9 +1,44 @@
 <?php
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_SESSION["login"]) && isset($_FILES["avatar"])) {
+    $tmpName = $_FILES['avatar']['tmp_name'];
+    $name = $_FILES['avatar']['name'];
+    $size = $_FILES['avatar']['size'];
+    $error = $_FILES['avatar']['error'];
+
+    $extensions = ['jpg', 'png', 'jpeg'];
+    $tabExtension = explode('.', $name);
+    $extension = strtolower(end($tabExtension));
+    
+    $maxSize = 400000;
+    
+    if(in_array($extension, $extensions) && $size <= $maxSize){
+        move_uploaded_file($tmpName, __DIR__.'../upload/avatar/user_'.$_SESSION["id"].'/'.$name);
+
+        try{
+            $sql = $db->prepare(
+                '
+                update user
+                set avatar = ?
+                where id = ? limit 1
+                '
+                );
+            $sql->execute(array($name, $_SESSION["id"]));
+        }
+        catch (\PDOException $e){
+            $data["result"] = $e->getMessage();
+        }
+    }
+    else{
+        echo "Mauvaise extension ou taille trop grande";
+        exit;
+    }
+}
+
 try{
     $sql = $db->prepare(
         '
-        select login, avatar from `user`
+        select id, login, avatar from `user`
         where `user`.id = ? limit 1
         '
         );
@@ -14,6 +49,13 @@ catch (\PDOException $e){
     $data["result"] = $e->getMessage();
 }
 
+$current_user = false;
+
+if (isset($_SESSION['id'])){
+    $current_user = $user["id"] == $_SESSION["id"];
+}
+
 render("user", [
     "user" => $user,
+    "current_user" => $current_user,
 ]);
--- www/views/user.php
+++ www/views/user.php
@@ -1,1 +1,12 @@
 <h2>Profil de <?= $data["user"]["login"] ?></h2>
+
+<img width="100px" height="100px" src="<?= $data["user"]["avatar"] === "" ? $data["user"]["avatar"] : "https://st.depositphotos.com/1598598/4847/i/950/depositphotos_48475325-stock-photo-isolated-plane-tree-on-a.jpg" ?>">
+
+<?php if ($data["current_user"]) { ?>
+<form method="post" enctype="multipart/form-data">
+    <h3>Modifier votre photo de profil</h3>
+    <label for="avatar">Sélectionner le fichier à envoyer</label>
+    <input type="file" id="avatar" name="avatar" accept="image/png, image/jpeg">
+    <input type="submit">
+</form>
+<?php } ?>
